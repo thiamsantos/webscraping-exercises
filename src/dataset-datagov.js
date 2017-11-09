ecd3a999c051646d4b648eb357e398eda6dfe45d
Thiago <thia.mdossantos@gmail.com>
2017-11-09 19:12:07 -0200

    :sparkles: add dataset data.gov exercise

---
+++ src/dataset-datagov.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer')
+const _ = require('lodash')
+
+// Number of datasets currently listed on data.gov
+async function main() {
+  try {
+    const browser = await puppeteer.launch({headless: false})
+    const page = await browser.newPage()
+    await page.goto('https://catalog.data.gov/dataset')
+    const content = await page.evaluate(async () => {
+      return document.querySelector('.new-results').textContent
+    })
+
+    await browser.close()
+
+    const result = parseInt(
+      _.trim(content)
+        .split(' ')[0]
+        .replace(',', '')
+    )
+
+    console.log(result)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
